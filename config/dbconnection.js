3cb04fc54bd98d673f0dab3db2537396d87c7d99
leandrocunha526 <leandrocunha526@gmail.com>
2019-05-01 18:18:43 -0300

    Deletando cadastroDAO do model

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 const connMySQL = function(){
     return connection = mysql.createConnection({
-        host: 'localhost', //AWS instance
-        user: 'root',
-        port: '3306',
-        password: 'victoradmin',
-        database: 'auma' 
+        host: 'localhost', //Host
+        user: 'root', //user
+        port: '3306', //port
+        password: 'victoradmin', //senha do host
+        database: 'auma'  //name of database 
     })
 }
 
