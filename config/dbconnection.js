cb968a5489ba895eb724c4fdf5ccb3eac26af77d
leandrocunha526 <leandrocunha526@gmaiil.com>
2019-05-01 20:14:30 -0300

    Inserindo dados da instância da aws (tabelas já inseridas)

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 const connMySQL = function(){
     return connection = mysql.createConnection({
-        host: 'localhost', //Host
-        user: 'root', //user
-        port: '3306', //port
-        password: 'victoradmin', //senha do host
-        database: 'auma'  //name of database 
+        host: 'projectpi.cvubxoiqjjki.us-east-1.rds.amazonaws.com', //Host da Intância AWS
+        user: 'root', //usuário da Intância AWS
+        port: '3306', //porta da Intância AWS
+        password: 'password_', //senha do host da Intância AWS
+        database: 'auma'  //nome da database da Intância AWS
     })
 }
 
