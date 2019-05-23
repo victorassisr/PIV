15f8a926c5131d279c5f8f0f7dc083b17063c525
Leandro Cunha <leandrocunha526@gmail.com>
2019-05-23 20:16:29 -0300

    Update BagDAO

--- app/models/BagDAO.js
+++ app/models/BagDAO.js
@@ -3,7 +3,7 @@ function BagDAO(connection) {
 }
 
 BagDAO.prototype.salvar_bag = function (bag, callback) {
-    this._connection.query('INSERT INTO noticias SET ?', bag, callback);
+    this._connection.query('INSERT INTO bag SET ?', bag, callback);
 }
 
 module.exports = function(){
